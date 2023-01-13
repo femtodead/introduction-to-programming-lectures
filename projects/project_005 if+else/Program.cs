5f84d1359da0ce5d6591e73ed57beb91631a97e0
Александр <femt0dead3@mail.ru>
2023-01-13 19:06:19 +0300

    conditional progra_name

---
+++ projects/project_005 if+else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "саша")
+{
+    Console.WriteLine("Привет САНЕК, ты лучший!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
