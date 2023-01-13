84a5437fdc0cb926bd1d82269968084c0d6647a5
Александр <femt0dead3@mail.ru>
2023-01-13 19:38:04 +0300

    kettlebell

---
+++ projects/prject_006_ kettlebell task/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 3;
+int b= 4;
+int c = 5;
+int z = 6;
+int d = 1;
+int max = a;
+
+if (max < a) max = a;
+if (max < b) max = b;
+if (max < c) max = c;
+if (max < z) max = z;
+if (max < d) max = d;
+
+Console.WriteLine(max);
