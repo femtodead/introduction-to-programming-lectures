6e204480e9a5cf9a4d1fb8f3015bdeb53bd07149
Александр <femt0dead3@mail.ru>
2023-01-13 18:23:36 +0300

    project_003 sum random numbers

--- projects/project_003_sum/Program.cs
+++ projects/project_003_sum/Program.cs
@@ -1,5 +1,4 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1, 10); //1 2 3 4 5 6 7 8 9
+int numberB = new Random().Next(1, 10); //1 2 3 4 5 6 7 8 9
 int sum = numberA + numberB;
-Console.WriteLine(numberA+numberB);
-Console.WriteLine(sum);
+Console.WriteLine(sum); 
